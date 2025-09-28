a4c26d375c5684900d3513978c5ff677d6caecd1
danhtranhuu0501 <danhtranhuu0501@gmail.com>
2025-09-28 21:14:23 +0700

    Update MongooseUtil.js

--- server/utils/MongooseUtil.js
+++ server/utils/MongooseUtil.js
@@ -14,8 +14,6 @@ console.log('User:', MyConstants.DB_USER);
 const options = {
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferMaxEntries: 0, // Disable mongoose buffering
-  bufferCommands: false, // Disable mongoose buffering
 };
 
 mongoose.connect(uri, options)
